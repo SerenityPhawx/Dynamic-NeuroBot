718281aa2fba5706de8ee3d46e2565baabcd3d62
Rudolf Zeidler <rudi@rzeidler.eu>
2014-10-25 21:51:32 +0200

    Modified readData.py

--- machineLearning/readData.py
+++ machineLearning/readData.py
@@ -1,12 +1,10 @@
 import numpy as np
-
-INPUT_FILE = "data.csv"
    
-def readInput(filename = INPUT_FILE, sensor1ID = 1, sensor2ID = 2):
+def readInput(filename, sensor1ID = 1, sensor2ID = 2):
    XInput = np.recfromcsv(INPUT_FILE)
    
-   X1 = X[:, np.where(XInput[:,0] == sensor1ID)]
-   X2 = X[:, np.where(XInput[:,0] == sensor2ID)]
+   X1 = X[:, np.where(XInput[:,0] == sensor1ID)[0]]
+   X2 = X[:, np.where(XInput[:,0] == sensor2ID)[0]]
    N = np.min(X1.shape[0], X2.shape[0])
    
    X1 = X1[:N-1,:]
