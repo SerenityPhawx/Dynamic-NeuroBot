ac5402a464109ff4623667402d5535d09ae287a3
Rudolf Zeidler <rudi@rzeidler.eu>
2014-10-25 18:21:48 +0200

    added preliminary file to do the time-frequency stuff.

---
+++ machineLearning/timeFreq.py
@@ -0,0 +1,16 @@
+import scipy
+import numpy as np
+
+SAMPLING_RATE = 250
+WINDOW_LENGTH=64
+
+
+def fftBins(signal):
+   ft = np.fft.rfft(signal * np.hamming(signal.shape[1]))
+   return(ft)
+
+
+def timeFreq(X):
+   X = X #padd with zeros to have shapes match up 
+   Y = np.reshape(X, newshape=(-1,WINDOW_LENGTH))
+   return(fftBins(Y))
